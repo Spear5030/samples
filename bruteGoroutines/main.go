f50e46ff97e08b40b4de9bd96510f3b389df1cb9
Spear5030 <spear5030@gmail.com>
2023-03-23 23:38:11 +0300

    Sample Bruteforces async with wg and done channel

--- bruteGoroutines/main.go
+++ bruteGoroutines/main.go
@@ -3,32 +3,45 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"sync"
 	"time"
 )
 
-const length = 6
+const length = 8
 
-var pwd = []byte{'z', '1', 'y', '2', '2', '4'}
-var abc = []byte{'x', 'y', 'z', '1', '2', '3', '4'}
+var pwd = []byte{'z', '1', 'y', '2', '2', '4', '5'}
+var abc = []byte{'x', 'y', 'z', '1', '2', '3', '4', '5'}
 var queue chan []byte
+var done chan struct{}
+var wg sync.WaitGroup
 
 func main() {
 	start := time.Now()
 	queue = make(chan []byte, 10)
+	done = make(chan struct{})
 	go makeQueue(nil)
-	go func() {
-		for {
-			select {
-			case x := <-queue:
-				if bytes.Equal(pwd, x) {
-					fmt.Println("Bingo ", string(x))
-					fmt.Println(time.Since(start))
-					break
-				}
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go worker(&wg, done)
+	}
+	wg.Wait()
+	fmt.Println(time.Since(start))
+}
+
+func worker(wg *sync.WaitGroup, done chan struct{}) {
+	for {
+		select {
+		case x := <-queue:
+			if bytes.Equal(pwd, x) {
+				fmt.Println("Bingo ", string(x))
+				close(done)
 			}
+		case <-done:
+			wg.Done()
+			return
 		}
-	}()
-	time.Sleep(time.Second * 10)
+	}
 }
 
 func makeQueue(pref []byte) {
